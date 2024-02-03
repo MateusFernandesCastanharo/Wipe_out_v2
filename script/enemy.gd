e832de9e7c9b5288ca8d24f8c354e17e7371efc0
lucastso10 <lucastso10@gmail.com>
2024-02-02 21:38:10 -0300

    consertei o fundo movendo mais rapido que o jogador

--- script/enemy.gd
+++ script/enemy.gd
@@ -5,8 +5,6 @@ const SPEED = 300.0
 
 func _physics_process(delta):
 	var player = get_tree().get_nodes_in_group("player")[0]
-	position += (player.position - position)/50
-	
 	
 	look_at(player.position)
 	self.rotation_degrees += 90
